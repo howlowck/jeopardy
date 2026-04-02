1134e45dafa3c910710898f4dd3ebd3a2c775a34
Selina Li <selina.li@microsoft.com>
2026-04-02 12:14:53 +1100

    Change in progress and to be tested

--- src/components/QuestionView/index.tsx
+++ src/components/QuestionView/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactModal from 'react-modal'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { close } from '../../redux/slices/questionModal'
 import { Team } from '../../types'
 import TeamView from '../TeamView'
@@ -10,6 +10,7 @@ import './styles.css'
 type Prop = {
   isOpen: boolean
   question: string
+  answer: string
   points: number
   teams: Team[]
   dailyDouble: boolean
@@ -19,63 +20,84 @@ type Prop = {
 const Component: React.FC<Prop> = ({
   isOpen,
   question,
+  answer,
   points,
   teams,
   dailyDouble,
   confirmedTeamIndex: confirmedTeam,
 }) => {
   const dispatch = useDispatch()
+  const [showAnswer, setShowAnswer] = useState(false)
+
+  useEffect(() => {
+    if (!isOpen) setShowAnswer(false)
+  }, [isOpen])
+
+  const questionFontSize =
+    question.length > 200 ? '4vh' :
+    question.length > 130 ? '6vh' :
+    question.length > 70  ? '8vh' : '10vh'
 
   return (
-			<ReactModal
-				className="modal question-view"
-				isOpen={isOpen}
-				overlayClassName="overlay"
+		<ReactModal
+			className="modal question-view"
+			isOpen={isOpen}
+			overlayClassName="overlay"
+		>
+			<i
+				className="material-icons close"
+				onClick={() => {
+					dispatch(close());
+				}}
 			>
-				<i
-					className="material-icons close"
-					onClick={() => {
-						dispatch(close());
-					}}
+				close
+			</i>
+			<div className="timer question-timer">
+				<CountdownCircleTimer
+					duration={20}
+					colors="#ffffff"
+					trailColor="#060CE9"
+					isPlaying
+					size={80}
 				>
-					close
-				</i>
-				<div className="timer question-timer">
-					<CountdownCircleTimer
-						duration={20}
-						colors="#ffffff"
-						trailColor="#060CE9"
-						isPlaying
-						size={80}
-					>
-						{({ remainingTime }) => remainingTime}
-					</CountdownCircleTimer>
-				</div>
+					{({ remainingTime }) => remainingTime}
+				</CountdownCircleTimer>
+			</div>
 
-				<div className="question">
-					<span>{question}</span>
+			<div className="question" style={{ fontSize: questionFontSize }}>
+				<span>{question}</span>
+			</div>
+			{showAnswer && (
+				<div className="answer">
+					<span>{answer}</span>
 				</div>
-				<div className="teams">
-					{dailyDouble && typeof confirmedTeam === "number"
-						? // only show the confirmed team for daily double
-							teams
-								.filter((_, i) => i === confirmedTeam)
-								.map((t, i) => (
-									<TeamView
-										points={points}
-										team={t}
-										teamIndex={confirmedTeam}
-										key={confirmedTeam}
-									/>
-								))
-						: teams.map((_, i) => {
-								return (
-									<TeamView points={points} team={_} teamIndex={i} key={i} />
-								);
-							})}
-				</div>
-			</ReactModal>
-		);
+			)}
+			<div className="show-answer-row">
+				<button className="show-answer-btn" onClick={() => setShowAnswer((v) => !v)}>
+					{showAnswer ? 'Hide Answer' : 'Show Answer'}
+				</button>
+			</div>
+			<div className="teams">
+				{dailyDouble && typeof confirmedTeam === "number"
+					? // only show the confirmed team for daily double
+						teams
+							.filter((_, i) => i === confirmedTeam)
+							.map((t, i) => (
+								<TeamView
+									points={points}
+									team={t}
+									teamIndex={confirmedTeam}
+									key={confirmedTeam}
+								/>
+							))
+					: teams.map((_, i) => {
+							return (
+								<TeamView points={points} team={_} teamIndex={i} key={i} />
+							);
+						})}
+			</div>
+		</ReactModal>
+	);
 }
 
 export default Component
